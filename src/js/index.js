f26de77932001702ed38b95ed9ab042e22810554
evgenia-chatzi <evgenia.chatzi@gig.com>
2020-04-30 12:01:53 +0200

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+import Search from './models/Search'
+import Recipe from './models/Recipe'
+import * as searchView from './views/searchView'
+import { elements, renderLoader, clearLoader } from './views/base'
+
+/** Global State of the App
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {}
+
+/**
+ * SEARCH CONTROLLER
+ */
+
+const controlSearch = async () => {
+    //1.Get the query from View
+    const query = searchView.getInput();
+
+
+    if (query) {
+        //2.New search object and add it to State
+        state.search = new Search(query);
+
+        //3.Prepare UI for the results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        //4.Search for recipes
+
+        await state.search.getResult();
+
+        //5. Render results on UI after the API returns the results
+        clearLoader();
+        searchView.renderResults(state.search.result);
+
+    }
+}
+
+elements.searchButton.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage)
+    }
+})
+
+const search = new Search('pizza');
+console.log(search)
+
+/**
+ * RECIPE CONTROLLER
+ */
+
+const r = new Recipe(54454);
+r.getRecipe();
+console.log(r);
+
+
